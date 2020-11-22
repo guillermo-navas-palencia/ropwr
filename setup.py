f1d99e3662ae80811f5b7a59ebbab90decb90d59
GUI <g.navas.palencia@gmail.com>
2020-11-22 18:53:19 +0100

    Basic setup file

---
+++ setup.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+from setuptools import find_packages, setup, Command
+from setuptools.command.test import test as TestCommand
+
+
+long_description = '''
+
+Read the documentation at: http://gnpalencia.org/ropwr/
+
+RoPWR is distributed under the Apache Software License (Apache 2.0).
+'''
+
+
+class CleanCommand(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        os.system('rm -vrf ./build ./dist ./*.pyc ./*.tgz ./*.egg-info')
+
+
+# test suites
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = []
+
+    def run_tests(self):
+        # import here, because outside the eggs aren't loaded
+        import pytest
+        errcode = pytest.main(self.test_args)
+        sys.exit(errcode)
+
+
+# install requirements
+install_requires = [
+    'cvxpy>=1.0',
+    'numpy>=1.16',
+    'scikit-learn>=0.22',
+]
+
+# test requirements
+tests_require = [
+    'pytest',
+    'coverage'
+]
+
+
+setup(
+    name="ropwr",
+    version="0.1.0",
+    description="RoPWR: Robust Piecewise Regression",
+    long_description=long_description,
+    author="Guillermo Navas-Palencia",
+    author_email="g.navas.palencia@gmail.com",
+    packages=find_packages(),
+    platforms="any",
+    include_package_data=True,
+    license="Apache Licence 2.0",
+    url="https://github.com/guillermo-navas-palencia/ropwr",
+    cmdclass={'clean': CleanCommand, 'test': PyTest},
+    python_requires='>=3.6',
+    install_requires=install_requires,
+    tests_require=tests_require,
+    classifiers=[
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3']
+    )
