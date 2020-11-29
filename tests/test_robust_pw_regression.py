aa43fe03bd9c5394da042cc94067faea19ea8bd6
GUI <g.navas.palencia@gmail.com>
2020-11-29 12:04:30 +0100

    Test params completed

--- tests/test_robust_pw_regression.py
+++ tests/test_robust_pw_regression.py
@@ -24,7 +24,53 @@ y = data.target
 
 
 def test_params():
-    pass
+    with raises(ValueError):
+        pw = RobustPWRegression(objective="l0")
+        pw.fit(x, y, splits=[5, 10, 15])
+
+    with raises(ValueError):
+        pw = RobustPWRegression(regularization="l0")
+        pw.fit(x, y, splits=[5, 10, 15])
+
+    with raises(ValueError):
+        pw = RobustPWRegression(degree=7)
+        pw.fit(x, y, splits=[5, 10, 15])
+
+    with raises(TypeError):
+        pw = RobustPWRegression(continuous=1)
+        pw.fit(x, y, splits=[5, 10, 15])
+
+    with raises(ValueError):
+        pw = RobustPWRegression(monotonic_trend="new")
+        pw.fit(x, y, splits=[5, 10, 15])
+
+    with raises(ValueError):
+        pw = RobustPWRegression(monotonic_trend="convex", continuous=False)
+        pw.fit(x, y, splits=[5, 10, 15])
+
+    with raises(ValueError):
+        pw = RobustPWRegression(solver=None)
+        pw.fit(x, y, splits=[5, 10, 15])
+
+    with raises(ValueError):
+        pw = RobustPWRegression(h_epsilon=0.9)
+        pw.fit(x, y, splits=[5, 10, 15])
+
+    with raises(ValueError):
+        pw = RobustPWRegression(quantile=0)
+        pw.fit(x, y, splits=[5, 10, 15])
+
+    with raises(ValueError):
+        pw = RobustPWRegression(reg_l1=-0.5)
+        pw.fit(x, y, splits=[5, 10, 15])
+
+    with raises(ValueError):
+        pw = RobustPWRegression(reg_l2=-0.5)
+        pw.fit(x, y, splits=[5, 10, 15])
+
+    with raises(TypeError):
+        pw = RobustPWRegression(verbose=1)
+        pw.fit(x, y, splits=[5, 10, 15])
 
 
 def test_splits():
@@ -40,4 +86,20 @@ def test_continuous():
 
 
 def test_discontinuous():
+    pass
+
+
+def test_solver_auto():
+    pass
+
+
+def test_solver_direct():
+    pass
+
+
+def test_solver_osqp():
+    pass
+
+
+def test_solver_ecos():
     pass
