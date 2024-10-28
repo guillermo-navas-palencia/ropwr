a143f5cff5646360ffce8cdef972abdc1c4d5757
GUI <g.navas.palencia@gmail.com>
2024-10-28 20:49:40 +0100

    Version 1.1.0

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -18,13 +18,13 @@ sys.path.insert(0, os.path.abspath('../..'))
 # -- Project information -----------------------------------------------------
 
 project = 'ropwr'
-copyright = '2020 - 2022, Guillermo Navas-Palencia'
+copyright = '2020 - 2024, Guillermo Navas-Palencia'
 author = 'Guillermo Navas-Palencia'
 
 # The short X.Y version
-version = '1.0.0'
+version = '1.1.0'
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '1.1.0'
 
 
 # -- General configuration ---------------------------------------------------
